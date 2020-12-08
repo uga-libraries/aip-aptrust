6816aa00f199cf87030d51515b19d016e83453ac
Adriane Hanson <ahansonuga@gmail.com>
2020-12-08 12:39:51 -0500

    Add undo bag function from another script

--- aptrust_aip.py
+++ aptrust_aip.py
@@ -4,6 +4,8 @@
 # To determine: APTrust does not maintain versions. If we submit another AIP with the same ID, just a later version,
 # it overwrites the original. Is that desired or do we add version number to the AIP ID?
 
+import os
+
 # UNPACKAGE THE AIP
 
 # Unzip and untar. Result is a bag.
@@ -23,7 +25,29 @@
 
 # No file  or directory name can exceed 255 characters, including extension.
 
-# If anything was changed, update the bag.
+# If anything was changed, undo and redo the bag.
+def undo_bag(bag):
+    """Copied from bag repo. Script for undoing a single bag. Untested with this script."""
+
+    # Change to the directory that is being unbagged.
+    os.chdir(bag)
+
+    # Delete the bag metadata files, which are all text files.
+    for doc in os.listdir('.'):
+        if doc.endswith('.txt'):
+            os.remove(doc)
+
+    # Move the contents from the data folder into the parent directory.
+    for item in os.listdir('data'):
+        os.replace(f'data/{item}', item)
+
+    # Delete the now-empty data folder.
+    os.rmdir('data')
+
+    # Delete '_bag' from the end of the directory name if the standard bag naming convention was used.
+    if bag.endswith('_bag'):
+        new_name = bag.replace('_bag', '')
+        os.replace(bag, new_name)
 
 
 # UPDATE THE BAG METADATA
