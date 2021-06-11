a9a545051549c20f80f75f95165654f3893a1e62
Adriane Hanson <ahansonuga@gmail.com>
2021-06-11 11:23:23 -0400

    Return values needed for script from validate_arguments()
    Finishes this function.

--- aptrust_upload.py
+++ aptrust_upload.py
@@ -10,7 +10,6 @@ def validate_arguments(arguments_list):
     tools, a path to the AIPs directory, and if the AIPs are to be uploaded to production or demo. If any are missing
     or not an expected value, prints an error message and quits the script. If they are present, makes the AIPs
     directory the current directory and calculates additional paths. """
-    # TODO: return some of these variables to use throughout the script.
 
     # The script usage information is used in many error statements.
     script_usage = "Script usage: python C:/path/batch_validate.py aptrust_type(production|demo) C:/path/aips_directory C:/path/partner_tools"
@@ -37,20 +36,23 @@ def validate_arguments(arguments_list):
 
     # Make paths to specific tools and files with the provided path to the partner tools.
     # Exits the script if any do not exist.
-    tools = arguments_list[3]
-    apt_validate = os.path.join(tools, "apt_validate.exe")
-    apt_upload = os.path.join(tools, "apt_upload.exe")
-    config_validate = os.path.join(tools, "aptrust_bag_validation_config.json")
-    credentials = os.path.join(tools, f"{aptrust_type}.conf")
+    tools_path = arguments_list[3]
+    apt_validate_path = os.path.join(tools_path, "apt_validate.exe")
+    apt_upload_path = os.path.join(tools_path, "apt_upload.exe")
+    config_validate_path = os.path.join(tools_path, "aptrust_bag_validation_config.json")
+    credentials_path = os.path.join(tools_path, f"{aptrust_type}.conf")
 
-    for path in (tools, apt_validate, apt_upload, config_validate, credentials):
+    for path in (tools_path, apt_validate_path, apt_upload_path, config_validate_path, credentials_path):
         if not os.path.exists(path):
             print(f"{path} is incorrect.\n{script_usage}")
             exit()
 
+    return apt_validate_path, apt_upload_path, config_validate_path, credentials_path
 
-# Validate script arguments.
-validate_arguments(sys.argv)
+
+# Validates the script arguments and quits the script if there are any errors.
+# Otherwise, changes the current directory to the AIPs directory the current directory, and gets tool and file paths.
+apt_validate, apt_upload, config_validate, credentials = validate_arguments(sys.argv)
 
 # Start tracking counts for summarizing script results.
 
