c918eb36f1fb3163ce15a6620de874b69fd21a3c
Adriane Hanson <ahansonuga@gmail.com>
2021-06-11 11:04:22 -0400

    Add make AIPs dir the current dir, validate_arguments()

--- aptrust_upload.py
+++ aptrust_upload.py
@@ -1,6 +1,7 @@
 # Use APTrust Partner Tools to validate and upload a batch of AIPs which are all in the same folder (AIPs directory).
 # Script usage: python C:/path/batch_validate.py aptrust_type(production|demo) C:/path/aips_directory C:/path/partner_tools
 
+import os
 import sys
 
 
@@ -9,6 +10,7 @@ def validate_arguments(arguments_list):
     tools, a path to the AIPs directory, and if the AIPs are to be uploaded to production or demo. If any are missing
     or not an expected value, prints an error message and quits the script. If they are present, makes the AIPs
     directory the current directory and calculates additional paths. """
+    # TODO: return some of these variables to use throughout the script.
 
     # The script usage information is used in many error statements.
     script_usage = "Script usage: python C:/path/batch_validate.py aptrust_type(production|demo) C:/path/aips_directory C:/path/partner_tools"
@@ -19,13 +21,19 @@ def validate_arguments(arguments_list):
         exit()
 
     # Exits the script if the provided APTrust type is not one of the two expected values, production or demo.
-    aptrust_type = sys.argv[1]
+    aptrust_type = arguments_list[1]
     if aptrust_type not in ("production", "demo"):
         print(f'The APTrust type must be "production" or "demo".\n{script_usage}')
         exit()
 
     # Makes the provided path to the AIPs directory the current directory.
     # Exits the script if it does not exist or it is a file instead of a directory.
+    aips_directory = arguments_list[2]
+    try:
+        os.chdir(aips_directory)
+    except (FileNotFoundError, NotADirectoryError):
+        print(f"The AIPs directory path is incorrect.\n{script_usage}")
+        exit()
 
     # Make paths to specific tools and files with the provided path to the partner tools.
     # Exits the script if any do not exist.
